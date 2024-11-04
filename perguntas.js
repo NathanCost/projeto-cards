9084c8afbd0b4349d4223da7984b270601330422
NathanCost <161321207+NathanCost@users.noreply.github.com>
2024-11-04 08:08:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,44 @@
+criaCartao(
+    'Modelo',
+    'Qual a modelo mais bem paga do mundo?',
+    'Gisele Bundchen é a modelo mais bem paga'
+)
+
+criaCartao(
+    'Designers',
+    'Qual o designer de moda mais influente na atualidade?',
+    'Miuccia Prada, diretora criativa das marcas Prada e Miu Miu'
+)
+
+criaCartao(
+    'Marcas de Luxo',
+    'Quais as principais marcas de luxo?',
+    'Estão entre as principais marcas de liuxo: Louis Vitton, Hermès e Dior'
+)
+
+criaCartao(
+    'Revistas',
+    'Qual a revista de maior visibilidade?',
+    'Revista VOGUE'
+)
+
+criaCartao(
+    'Brasileiras',
+    'Modelos brasileiros mais relevantes?',
+    'Adriana Lima, Chico Lachowski e Alessandra Ambrósio'
+)
+criaCartao(
+    'Desfiles',
+    'Quais os desfiles mais conhecidos?',
+    'New York Fashion Week e Milan Feshion Week'
+)
+criaCartao(
+    'Angenls',
+    'Quais as Angels mais duradouras da Victoria s Secret?',
+    'Adriana Lima, Alessandra Ambrósio, Tyra Banks e Candice Swanepoel'
+)
+criaCartao(
+    'victoria s Secret',
+    'O que é a Victoria s Secret?',
+    'Victortia s Secret é uma marca americada de ligerie'
+)
